6a9dde75dd1b99fdade6b2512655689ad9c05d60
newage99 <rooger99@gmail.com>
2020-04-30 20:45:30 +0200

    Converted IdGenerator to class.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from ExpressionInterpreter import ExpressionInterpreter
-from IdMutator import random_id, mutate_id
+from IdGenerator import IdGenerator
+from IdMutator import IdMutator
 
 if __name__ == '__main__':
     ei = ExpressionInterpreter()
     result = ei.compute("21^405")
     x = 0
-    r = random_id(12)
+    r = IdGenerator.generate_id(12)
     print(r)
     print('')
+    id_mutator = IdMutator()
     while x < 300:
-        id, pos, c, new_c = mutate_id(r)
+        id, pos, c, new_c = id_mutator.mutate_id(r, len(r))
         print(id + '  pos_to_mutate=' + str(pos) + '(' + c + '), mutated_to=' + new_c)
         x += 1
